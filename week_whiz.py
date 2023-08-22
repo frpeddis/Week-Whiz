8bc26805d39d32dc94b22ab32d5d36a65577705a
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-22 23:01:47 +0200

    Update week_whiz.py

--- week_whiz.py
+++ week_whiz.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 
 st.set_page_config(page_title="🌀 Guess the day - F Peddis")
-st.title("The initial date is all you need...")
+st.title(":rocket: The initial date is all you need...")
 st.header("Select your challenge from the sidebar on the left ...")
 st.sidebar.success("Select your challenge ")
