55bdab2b619c9287f17fd4f9b53c3ae304d9e446
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-22 23:10:42 +0200

    Update week_whiz.py

--- week_whiz.py
+++ week_whiz.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 st.set_page_config(page_title="🌀 Guess the day - F Peddis")
-st.title(":rocket: The initial date is all you need... Select your challenge from the sidebar on the left ")
+st.title(":rocket: Welcome !!! The initial date is all you need... Select your challenge from the sidebar on the left ")
 com.iframe("https://lottie.host/?file=380d3ff9-0c30-4a96-b25b-7eeb8868bfeb/vnvhMZFQ8j.json")
 st.write("Select your challenge from the sidebar on the left ...")
 st.sidebar.success("Select your challenge ")
