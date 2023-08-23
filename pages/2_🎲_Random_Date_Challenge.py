ebc0fa095e053bdfb31105615db2d232216f0f1f
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-23 23:12:39 +0200

    Update 2_🎲_Random_Date_Challenge.py

--- pages/2_🎲_Random_Date_Challenge.py
+++ pages/2_🎲_Random_Date_Challenge.py
@@ -149,7 +149,7 @@ if check_button:
         
             # Display calculated string
             calculated_string = f"**:green[{year_last_2_digits}]** + **:green[{year_divided_by_4}]** + **:green[{century_correction_value}]** + **:green[{month_coefficient}]** + **:green[{day_of_month}]**"
-            st.write(":point_right: Magic Sum: ", calculated_string, " = ", f"<span style='font-size:20px; font-weight:bold;'>{subtotal}</span>", unsafe_allow_html=True)
+            st.write(":point_right: Magic Sum: ", calculated_string, " = ", f"<span style='font-size:18px; font-weight:bold;'>{subtotal}</span>", unsafe_allow_html=True)
             
             #calculated_string = f"{year_last_2_digits} + {year_divided_by_4} + {century_correction_value} + {month_coefficient} + {day_of_month}"
             #st.write(":point_right: Magic Sum: ", calculated_string, " = ", f"<span style='font-size:18px; font-weight:bold;'>{subtotal}</span>", unsafe_allow_html=True)
