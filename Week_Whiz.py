6d4942abc331a2ecbb2d94455c0b7f5ea21a36c0
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-23 13:53:42 +0200

    Update Week_Whiz.py

--- Week_Whiz.py
+++ Week_Whiz.py
@@ -3,6 +3,6 @@ import streamlit as st
 
 st.set_page_config(page_title="🌀 Guess the day - F Peddis")
 st.title(":sunglasses: Welcome!!! ")
-st.header(":point_left: Select your Calendar Challenge from the sidebar on the left ...")
+st.header(":point_left: Your Calendar Challenge from the sidebar on the left ...")
 st.write("The right **day of the week** for any given **date**! Just 4 additions, 1 division and 2 simple tables to memorize. Let's give it a try! ")
 st.sidebar.success("Select your challenge ")
