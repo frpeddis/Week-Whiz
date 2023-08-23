2e2cb97c55ff38da6be8c61f363852a3af3ac744
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-23 23:38:01 +0200

    Update 0_🏠_Home.py

--- 0_🏠_Home.py
+++ 0_🏠_Home.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 
-st.set_page_config(page_title="🌀 Guess the right day! - by FP")
+st.set_page_config(page_title="🌀 Guess the weekday! - by FP")
 
 st.write("↖️ Your Calendar Challenge on upper left sidebar")
 
