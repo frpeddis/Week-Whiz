cb37ea633e97616f4f4a7b69750423a1a691c131
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-23 23:35:51 +0200

    Update 0_🏠_Home.py

--- 0_🏠_Home.py
+++ 0_🏠_Home.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="🌀 Guess the right day! - by FP")
 st.write("↖️ Your Calendar Challenge on upper left sidebar")
 
 st.title("Welcome!!!  :sunglasses:")
-st.header("🗓️ Guess the **day of the week** for any **date**!")
+st.header("🗓️ Guess the **weekday** for **any date!**")
 st.write("You just need simple basic math and a little bit of memory... ") 
 st.write("Let's give it a try!")
 
