9d71f5903f021631278bbf3ef6572d462a75f334
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-23 17:40:36 +0200

    Update 0_🏠_Home.py

--- 0_🏠_Home.py
+++ 0_🏠_Home.py
@@ -10,7 +10,7 @@ st.header("🗓️ Guess the **day of the week** for any **date**!")
 st.write("You just need simple basic math and a little bit of memory... ") 
 st.write("Let's give it a try!")
 
-st.header("↖️ Your Calendar Challenge from the sidebar up on the left")
+st.header("↖️ Your Calendar Challenge from the sidebar up on the left ")
 
  
 st.sidebar.success("Select your challenge ")
