dbdc8d64f1fc9386823d4853a083cf85565ed15f
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-23 15:02:10 +0200

    Update 3_Choose the day.py

--- pages/3_Choose the day.py
+++ pages/3_Choose the day.py
@@ -88,6 +88,126 @@ if check_button and not invalid_date:
     else:
         st.error(day_of_week + " ...sorry for that!!!")
         st.snow()
+        selected_date = st.session_state.random_date
+        
+        if selected_date:
+            description2 = "Focus on "
+            st.markdown(f"{description2} {value}")
+           
+
+            # Step 2: Take the last 2 digits of the year
+            year_last_2_digits = selected_date.year % 100
+        
+            # Step 3: Divide the year number by 4 and add it
+            year_divided_by_4 = year_last_2_digits // 4
+            subtotal = year_last_2_digits + year_divided_by_4
+        
+            # Step 4: Add the "Century Correction"
+            century_correction_table = {
+                "Century": [1500, 1600, 1700, 1800, 1900, 2000],
+                "Correction": [0, 6, 4, 2, 0, -1]
+            }
+            century = (selected_date.year // 100) * 100
+            century_correction_value = century_correction_table["Correction"][century_correction_table["Century"].index(century)]
+            subtotal += century_correction_value
+        
+            # Step 5: Add the "Month Coefficient"
+            month_coefficients = {
+                "January": 1 if not (selected_date.year % 4 == 0 and selected_date.month <= 2) else 0,
+                "February": 4 if not (selected_date.year % 4 == 0 and selected_date.month <= 2) else 3,
+                "March": 4, "April": 0, "May": 2, "June": 5,
+                "July": 0, "August": 3, "September": 6,
+                "October": 1, "November": 4, "December": 6
+            }
+            month = selected_date.strftime("%B")
+            month_coefficient = month_coefficients[month]
+            subtotal += month_coefficient
+        
+            # Step 6: Add the day of the month
+            day_of_month = selected_date.day
+            subtotal += day_of_month
+        
+            # Step 7: Divide the subtotal by 7 and find the remainder
+            remainder = subtotal % 7
+        
+            # Display calculated string
+            calculated_string = f"{year_last_2_digits} + {year_divided_by_4} + {century_correction_value} + {month_coefficient} + {day_of_month}"
+            st.write(":point_right: Magic Sum: ", calculated_string, " = ", f"<span style='font-size:18px; font-weight:bold;'>{subtotal}</span>", unsafe_allow_html=True)
+            #st.write("Magic Sum: ", calculated_string, " = ", subtotal)
+            
+            
+            # Step 2: Take the last 2 digits of the year (continued)
+            st.write(year_last_2_digits, ": Last 2 digits of the year YY")
+        
+            # Step 3: Divide the year number by 4 and add it (continued)
+            st.write(year_divided_by_4, ": YY divided by 4 (only integer part!)")
+            
+            # Step 4: Add the "Century Correction" (continued)
+            st.write(century_correction_value, ": Century Correction for ", century, " (little table below)")
+          
+            # Step 5: Add the "Month Coefficient" (continued)
+            st.write(month_coefficient, ": Month Coefficient for ", month, " (little table below)")
+        
+            # Step 6: Add the day of the month (continued)
+            st.write(day_of_month, ": Day of the month")
+            
+            # Step 7: Divide the subtotal by 7 and find the remainder (continued)
+            #st.write(":point_right: Remainder after dividing ", subtotal, "  by 7:", remainder)
+            st.write(":point_right: Remainder after dividing ", subtotal, "  by 7:", f"<span style='font-size:18px; font-weight:bold;'>{remainder}</span>", unsafe_allow_html=True)
+            
+            # Display Correspondence Table
+            #st.write("Correspondence between Remainders and Days of the Week Table:")
+            correspondence_table = {
+                "Remainder": list(range(7)),
+                "Day of the Week": ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+            }
+            formatted_correspondence_table = []
+            for r, d in zip(correspondence_table["Remainder"], correspondence_table["Day of the Week"]):
+                if r == remainder:
+                    formatted_correspondence_table.append(["**" + str(r) + "**", "**" + d + "**"])
+                else:
+                    formatted_correspondence_table.append([str(r), d])
+            df_correspondence = pd.DataFrame(formatted_correspondence_table, columns=["Reminder", "Day of the week"])
+            #st.write("Remainders and Days of the Week:")
+            st.dataframe(df_correspondence)
+            
+            
+            # Display Century Correction Table
+            st.write("Century Correction")
+            century_correction_table = {
+                "Century": [1500, 1600, 1700, 1800, 1900, 2000],
+                "Correction": [0, 6, 4, 2, 0, -1]
+            }
+            formatted_century_correction_table = []
+            for century, correction in zip(century_correction_table["Century"], century_correction_table["Correction"]):
+                if century == (selected_date.year // 100) * 100:
+                    formatted_century_correction_table.append(["**" + str(century) + "**", "**" + str(correction) + "**"])
+                else:
+                    formatted_century_correction_table.append([str(century), str(correction)])
+            df_century_correction = pd.DataFrame(formatted_century_correction_table, columns=["Century", "Correction"])
+            #st.write("Century Correction Table:")
+            st.dataframe(df_century_correction)
+        
+            
+            # Display Month Coefficient Table (continued)
+            st.write("Month Coefficient")
+            month_coefficients = {
+                "January": 1 if not (selected_date.year % 4 == 0 and selected_date.month <= 2) else 0,
+                "February": 4 if not (selected_date.year % 4 == 0 and selected_date.month <= 2) else 3,
+                "March": 4, "April": 0, "May": 2, "June": 5,
+                "July": 0, "August": 3, "September": 6,
+                "October": 1, "November": 4, "December": 6
+            }
+            formatted_month_coefficients_table = []
+            for month, coeff in month_coefficients.items():
+                if month == selected_date.strftime("%B"):
+                    formatted_month_coefficients_table.append(["**" + month + "**", "**" + str(coeff) + "**"])
+                else:
+                    formatted_month_coefficients_table.append([month, str(coeff)])
+            df_month_coefficients = pd.DataFrame(formatted_month_coefficients_table, columns=["Month", "Value"])
+            #st.write("Month Coefficient Table:")
+            st.dataframe(df_month_coefficients)
+
         #news_summary = generate_news(selected_date)
         #st.title("Please verify, but according to ChatGPT that day...")
         #st.write(news_summary)
