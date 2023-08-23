2118b90bee8dcc5a915b0b16ad597ecc6ec0cfa7
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-23 23:27:28 +0200

    Update 0_🏠_Home.py

--- 0_🏠_Home.py
+++ 0_🏠_Home.py
@@ -4,13 +4,13 @@ import streamlit as st
 
 st.set_page_config(page_title="🌀 Guess the right day! - by FP")
 
+st.subheader("↖️ Your Calendar Challenge from the sidebar up on the left ")
 
 st.title("Welcome!!!  :sunglasses:")
 st.header("🗓️ Guess the **day of the week** for any **date**!")
 st.write("You just need simple basic math and a little bit of memory... ") 
 st.write("Let's give it a try!")
 
-st.subheader("↖️ Your Calendar Challenge from the sidebar up on the left ")
 
  
 st.sidebar.success("Select your challenge ")
