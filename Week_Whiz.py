e59bbae667f28c563ef410e2e99f7fbffdb5606c
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-23 14:32:40 +0200

    Update Week_Whiz.py

--- Week_Whiz.py
+++ Week_Whiz.py
@@ -5,8 +5,9 @@ st.set_page_config(page_title="🌀 Guess the day - F Peddis")
 st.title("Welcome!!!  :sunglasses:")
 st.header("🗓️ Guess the **day of the week** for any **date**!")
 st.write("You just need simple basic math and a little bit of memory... ") 
+st.write("Let's give it a try!")
 
 st.header(":point_left: Your Calendar Challenge from the sidebar on the left ...")
 
-st.write("Let's give it a try!") 
+ 
 st.sidebar.success("Select your challenge ")
