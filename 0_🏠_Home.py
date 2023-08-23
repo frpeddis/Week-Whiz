144f2951573de5a88381496f21847d57870b5ec0
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-23 17:16:29 +0200

    Update 0_🏠_Home.py

--- 0_🏠_Home.py
+++ 0_🏠_Home.py
@@ -2,9 +2,6 @@ import streamlit as st
 
 
 st.set_page_config(page_title="🌀 Guess the right day! - by FP")
-image_url = "https://raw.githubusercontent.com/frpeddis/Week-Whiz/927ecd1e0f19513b2e0887675fecfe4da17577a7/Image%2023-08-23%20at%2017.09.jpeg"
-
-st.image(image_url, use_column_width=True)
 st.title("Welcome!!!  :sunglasses:")
 st.header("🗓️ Guess the **day of the week** for any **date**!")
 st.write("You just need simple basic math and a little bit of memory... ") 
