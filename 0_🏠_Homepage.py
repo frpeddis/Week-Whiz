55e963cdda55ace5193dcde7f50a56bc33d773e6
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-25 22:17:21 +0200

    Update 0_🏠_Homepage.py

--- 0_🏠_Homepage.py
+++ 0_🏠_Homepage.py
@@ -3,7 +3,6 @@
 import streamlit as st
 import streamlit.components.v1 as com
 
-
 st.set_page_config(page_title="🌀 Guess the Weekday!")
 
 st.subheader("↖️ Your Calendar Challenge on the upper left sidebar")
@@ -13,9 +12,7 @@ com.iframe("https://lottie.host/?file=5a47a9d0-e7b0-492a-a32c-dd53d0fbfd5b/NaMYq
 st.title("Welcome!!!  :sunglasses:")
 st.header("Guess the **weekday** for **any date!**")
 
-
 st.subheader("With a little practice you can calculate it in your head... ") 
-
 
  
 #st.sidebar.success("Select your challenge ")
