2fd8ea31c2bc55d4d95900d2f50bcb69e6aa4422
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-26 20:09:20 +0200

    Update 2_🎲_Random_Date_Challenge.py

--- pages/2_🎲_Random_Date_Challenge.py
+++ pages/2_🎲_Random_Date_Challenge.py
@@ -216,7 +216,8 @@ if check_button:
             #st.write(":point_right: Remainder after dividing ", subtotal, "  by 7:", remainder)
             time.sleep(3)
             #st.write(":point_right: Remainder after dividing the Magic Sum ", subtotal, "  by 7 ---> ", f"<span style='font-size:18px; font-weight:bold;'>{remainder}</span>", unsafe_allow_html=True)
-            st.write("Now **divide the Magic Sum by 7**. You don't have to worry about the result, you don't need it! Just look at the **remainder** of the division (a number between 0 and 6) ")
+            st.write("Now **divide the Magic Sum by 7**. You don't have to worry about the result, you don't need it!")
+            st.write("Just look at the **remainder** of the division (a number between 0 and 6) ")
             st.write("**Remainder** after dividing **Magic Sum** ", sbt, "  / **7** :point_right:", f"<span style='font-size:23px; font-weight:bold;'>{remainder}</span>", unsafe_allow_html=True)
             st.subheader("🙌 We did it !!!")
             
