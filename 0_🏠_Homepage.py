8148826e4e62c4eb54d65d8b09dcd10d142f852a
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-08-27 17:41:30 +0200

    Update 0_🏠_Homepage.py

--- 0_🏠_Homepage.py
+++ 0_🏠_Homepage.py
@@ -6,7 +6,7 @@ import time
 
 st.set_page_config(page_title="🌀 Guess the Weekday!")
 
-st.subheader("↖️ Your Calendar Challenge on the upper left sidebar")
+st.subheader("↖️ Your Calendar Challenge on the upper left sidebar ")
 
 com.iframe("https://lottie.host/?file=f8b3d68c-9de2-4a01-8c29-1bb24bbe72c6/Rafbgzfeae.json")
 
